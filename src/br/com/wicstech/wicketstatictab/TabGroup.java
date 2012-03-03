cca89fdc84b0239e8ec39cdf3ce48caeb0653b6d
sergiowww@gmail.com <sergiowww@gmail.com@3aa9af5b-438f-cf67-0ead-8c1e9cafd636>
2012-03-03 00:11:29 +0000

    Inclusão de método da api

--- src/br/com/wicstech/wicketstatictab/TabGroup.java
+++ src/br/com/wicstech/wicketstatictab/TabGroup.java
@@ -1,6 +1,8 @@
 package br.com.wicstech.wicketstatictab;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.IHeaderContributor;
@@ -12,20 +14,20 @@ import org.apache.wicket.markup.html.panel.Panel;
 
 /**
  * Grupo de abas.
+ * 
  * @author Sergio
- *
+ * 
  */
-public class TabGroup extends Panel implements IHeaderContributor{
+public class TabGroup extends Panel implements IHeaderContributor {
 	private static final long serialVersionUID = 7472888933369223925L;
-	
-	private static final ResourceReference JS_ABAS = new ResourceReference(TabGroup.class, "abas.js");
-	private Tab[] tabs;
 
-	
-	public TabGroup(String id, Tab ... tabs) {
+	private static final ResourceReference JS_ABAS = new ResourceReference(TabGroup.class, "abas.js");
+	private List<Tab> tabs;
+
+	public TabGroup(String id, Tab... tabs) {
 		super(id);
-		this.tabs = tabs;
-		add(new ListView<Tab>("botoesAbasIt", Arrays.asList(tabs)){
+		this.tabs = new ArrayList<Tab>(Arrays.asList(tabs));
+		add(new ListView<Tab>("botoesAbasIt", this.tabs) {
 			private static final long serialVersionUID = 7681016248892904914L;
 
 			@Override
@@ -39,6 +41,14 @@ public class TabGroup extends Panel implements IHeaderContributor{
 		});
 	}
 
+	/**
+	 * Adicionar mais abas.
+	 * 
+	 * @param tabs
+	 */
+	public void addTab(Tab... tabs) {
+		this.tabs.addAll(Arrays.asList(tabs));
+	}
 
 	public void renderHead(IHeaderResponse response) {
 		response.renderJavascriptReference(JS_ABAS, "abas");
